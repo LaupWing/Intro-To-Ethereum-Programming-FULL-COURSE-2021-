a5a01210beb7f7c3e75ee9d8d12e84a8fc3c46b9
LaupWing <loc_strict@hotmail.com>
2022-02-25 14:32:34 +0100

    1:32:05

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -20,6 +20,7 @@ contract TokenFarm{
 
    // 1. Stakes Tokens (Deposit)
    function stakeTokens(uint _amount) public{
+      require(_amount > 0, "amount cannot be 0");
       // Transfer Mock Dai tokens to this contract for staking
       daiToken.transferFrom(msg.sender, address(this), _amount);
 
@@ -35,7 +36,16 @@ contract TokenFarm{
       hasStaked[msg.sender] = true;
    }
 
-   // 2. Unstakeing Tokens (Withdraw)
+   // Unstakeing Tokens (Withdraw)
 
-   // 3. Issuing Tokens
+   // Issuing Tokens
+   function issueToken() public{
+      for(uint i=0; i <stakers.length; i ++){
+         address recipient = stakers[i];
+         uint balance = stakingBalance[recipient];
+         if(balance > 0){
+            dappToken.transfer(recipient, balance);
+         }
+      }
+   }
 }

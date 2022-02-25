d1f8fefb122e899a51a5d7f8e5fc6538efcbacb2
LaupWing <loc_strict@hotmail.com>
2022-02-25 14:23:02 +0100

    staking tests finished

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -8,8 +8,34 @@ contract TokenFarm{
    DappToken public dappToken;
    DaiToken public daiToken;
 
+   mapping(address => uint) public stakingBalance;
+   mapping(address => bool) public hasStaked;
+   mapping(address => bool) public isStaking;
+   address[] public stakers;
+
    constructor(DappToken _dappToken, DaiToken _daiToken) public{
       dappToken = _dappToken;
       daiToken = _daiToken;
    }
+
+   // 1. Stakes Tokens (Deposit)
+   function stakeTokens(uint _amount) public{
+      // Transfer Mock Dai tokens to this contract for staking
+      daiToken.transferFrom(msg.sender, address(this), _amount);
+
+      stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+      // Add user to stakers array *only* if the havent staked already
+      if(!hasStaked[msg.sender]){
+         stakers.push(msg.sender);        
+      }
+
+      // Update staking statuss
+      isStaking[msg.sender] = true;
+      hasStaked[msg.sender] = true;
+   }
+
+   // 2. Unstakeing Tokens (Withdraw)
+
+   // 3. Issuing Tokens
 }

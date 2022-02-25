b29c2c315cb3ee80a8cfe53f7593dc43fca47d67
LaupWing <loc_strict@hotmail.com>
2022-02-25 13:50:49 +0100

    Got an error

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -8,15 +8,27 @@ require('chai')
    .use(require('chai-as-promised'))
    .should()
 
+function tokens(n){
+   return web3.utils.toWei(n, 'ether')
+}
 
-contract('Tokenfarm', accounts =>{
+contract('Tokenfarm', ([owner, investor]) =>{
    // Write test here...
    let daiToken, dappToken, tokenFarm
 
    before(async ()=>{
+      // Load contracts
       daiToken = await DaiToken.new()
       dappToken = await DappToken.new()
       tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+
+      // Transfer all Dapp tokens to farm (1 million)
+      // await dappToken.transfer(tokenFarm.address, tokens('1000000'))
+
+      // await dappToken.transfer(investor, tokens('100'), {
+      //    from: owner
+      // })
+
    })
 
 

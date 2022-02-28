16eb7e97f425a8b6c611695d020de11298887ee5
LaupWing <loc_strict@hotmail.com>
2022-02-28 15:32:22 +0100

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,57 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
 import './App.css'
+import Web3 from 'web3'
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '0x0'
-    }
-  }
+   async componentDidMount(){
+      await this.loadWeb3()
+   }
 
-  render() {
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                </a>
+   async loadWeb3(){
+      if(window.ethereum){
+         window.web3 = new Web3(window.ethereum)
+         await window.ethereum.enable()
+      }
+      else if (window.web3){
+         window.web3 = new Web3(window.web3.currentProvider)
+      }else{
+         alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      }
+   }
 
-                <h1>Hello, World!</h1>
+   constructor(props) {
+      super(props)
+      this.state = {
+         account: '0x0'
+      }
+   }
 
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div>
+            <Navbar account={this.state.account} />
+            <div className="container-fluid mt-5">
+               <div className="row">
+                  <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
+                     <div className="content mr-auto ml-auto">
+                        <a
+                           href="http://www.dappuniversity.com/bootcamp"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                        </a>
+
+                        <h1>Hello, World!</h1>
+
+                     </div>
+                  </main>
+               </div>
+            </div>
+         </div>
+      );
+   }
 }
 
 export default App;

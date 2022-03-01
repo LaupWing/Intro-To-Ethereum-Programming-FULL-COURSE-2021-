1214f3a813a233c79e04886d76aadf3b9a1e4a66
LaupWing <loc_strict@hotmail.com>
2022-03-01 13:04:15 +0100

    loading

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Web3 from 'web3'
 import DaiToken from '../abis/DaiToken.json'
 import DappToken from '../abis/DappToken.json'
 import TokenFarm from '../abis/TokenFarm.json'
+import Main from './Main'
 
 class App extends Component {
 
@@ -92,6 +93,13 @@ class App extends Component {
    }
 
    render() {
+      let content
+      if(this.state.loading){
+         content = <p id='loader' className='text-center'>Loading...</p>
+      }else{
+         content = <Main/>
+      }
+
       return (
          <div>
             <Navbar account={this.state.account} />
@@ -105,9 +113,7 @@ class App extends Component {
                            rel="noopener noreferrer"
                         >
                         </a>
-
-                        <h1>Hello, World!</h1>
-
+                        {content}
                      </div>
                   </main>
                </div>

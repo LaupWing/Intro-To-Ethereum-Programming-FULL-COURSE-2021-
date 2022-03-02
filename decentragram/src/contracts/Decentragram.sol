156954aaaef3a13361a5973bf2eb5ef0e1b6c7ad
LaupWing <loc_strict@hotmail.com>
2022-03-02 13:45:18 +0100

    images

--- decentragram/src/contracts/Decentragram.sol
+++ decentragram/src/contracts/Decentragram.sol
@@ -2,5 +2,12 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Decentragram {
    string public name = "Decentragram";
-   
+   mapping(uint => Image) public images;
+   struct Image {
+      uint id; // uint is always positive
+      string hash;
+      string description;
+      uint tipAmount;
+      address payable author;
+   }
 }

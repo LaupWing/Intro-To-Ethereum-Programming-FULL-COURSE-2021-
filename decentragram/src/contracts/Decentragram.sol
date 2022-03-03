1de2a2dec11d6b03fea290cda6c4331195aff2ec
LaupWing <loc_strict@hotmail.com>
2022-03-03 17:28:01 +0100

    sender

--- decentragram/src/contracts/Decentragram.sol
+++ decentragram/src/contracts/Decentragram.sol
@@ -2,16 +2,21 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Decentragram {
    string public name = "Decentragram";
+
+   uint public imageCount = 0;
    mapping(uint => Image) public images;
+
    struct Image {
       uint id; // uint is always positive
       string hash;
       string description;
       uint tipAmount;
+      // address author;
       address payable author;
    }
 
-   function uploadImage() public{
-      images[1] = Image(1, 'abc', 'Hello World', 0, payable(address(0)));
+   function uploadImage(string memory _imageHash, string memory _describtion) public{
+      imageCount + imageCount++;
+      images[imageCount] = Image(imageCount, _imageHash, _describtion, 0, msg.sender);
    }
 }

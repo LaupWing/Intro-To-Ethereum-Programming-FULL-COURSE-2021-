f3f2500518db1d79809768118f609cdd1783cba7
LaupWing <loc_strict@hotmail.com>
2022-03-07 14:48:00 +0100

    tipImageOwner

--- decentragram/src/contracts/Decentragram.sol
+++ decentragram/src/contracts/Decentragram.sol
@@ -22,6 +22,14 @@ contract Decentragram {
       uint tipAmount,
       address author
    );
+
+   event ImageTipped(
+      uint id,
+      string hash,
+      string description,
+      uint tipAmount,
+      address author
+   );
       // address payable author
 
    function uploadImage(string memory _imageHash, string memory _description) public{
@@ -37,10 +45,13 @@ contract Decentragram {
    }
 
    function tipImageOwner(uint _id) public payable{
+      require(_id > 0 && _id <= imageCount);
       Image memory _image = images[_id];
       address payable _author = payable(_image.author);
       _author.transfer(msg.value);
       _image.tipAmount = _image.tipAmount + msg.value;
       images[_id] = _image;
+
+      emit ImageTipped(_id, _image.hash, _image.description, _image.tipAmount, _image.author);
    }
 }

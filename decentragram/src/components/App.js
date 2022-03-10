ff9202f9cd209ef9bae6acfbe3010cce2ec203d3
LaupWing <loc_strict@hotmail.com>
2022-03-10 15:48:10 +0100

    captureFile

--- decentragram/src/components/App.js
+++ decentragram/src/components/App.js
@@ -52,6 +52,19 @@ class App extends Component {
       }
    }
 
+   captureFile = event =>{
+      event.preventDefault()
+      const file = event.target.files[0]
+      const reader = new window.FileReader()
+      reader.readAsArrayBuffer(file)
+
+      reader.onloadend = ()=>{
+         this.setState({
+            buffer: Buffer(reader.result)
+         })
+      }
+   }
+
    constructor(props) {
       super(props)
       this.state = {

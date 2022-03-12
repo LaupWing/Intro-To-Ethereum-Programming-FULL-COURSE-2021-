edc5b9eccdab1133858d8571df682cf74044f8d7
LaupWing <loc_strict@hotmail.com>
2022-03-12 18:53:53 +0100

    before

--- blockchain_game/test/MemoryToken.test.js
+++ blockchain_game/test/MemoryToken.test.js
@@ -8,15 +8,23 @@ require('chai')
 
 contract('Memory Token', (accounts) => {
    let token
+
+   before(async ()=>{
+      token = await MemoryToken.deployed()
+   })
    // code goes here...
    describe('deployment', async ()=>{
       it('deploys succesfully', async ()=>{
-         token = await MemoryToken.deployed()
          const address = token.address
          assert.notEqual(address, 0x0)
          assert.notEqual(address, '')
          assert.notEqual(address, null)
          assert.notEqual(address, undefined)
+      })
+
+      it('has a name', async ()=>{
+         const name = await token.name()
+         assert.equal(name, 'Memory Token')
       })
    })
 })

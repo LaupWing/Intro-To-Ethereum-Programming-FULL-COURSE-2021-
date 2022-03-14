f287d5c4d6b03fdddb77be676180b87e2fb7cd03
LaupWing <loc_strict@hotmail.com>
2022-03-14 14:29:46 +0100

    update

--- dvideo/src/contracts/DVideo.sol
+++ dvideo/src/contracts/DVideo.sol
@@ -2,32 +2,43 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract DVideo {
-  uint public videoCount = 0;
-  string public name = "DVideo";
-  //Create id=>struct mapping
+   uint256 public videoCount = 0;
+   string public name = "DVideo";
+   mapping(uint => Video) public videos;
 
-  //Create Struct
+   // 1. Model the Video
+   //Create id=>struct mapping
+   struct Video {
+      uint id;
+      string hash;
+      string title;
+      address author;
+   }
 
+   //Create Struct
 
-  //Create Event
+   //Create Event
+   event VideoUploaded(
+      uint id,
+      string hash,
+      string title,
+      address author
+   );
 
+   constructor() public {}
 
-  constructor() public {
-  }
+   function uploadVideo(string memory _videoHash, string memory _title)
+      public
+   {
+      // Make sure the video hash exists
+      // Make sure video title exists
+      // Make sure uploader address exists
+      // Increment video id
+      videoCount++;
 
-  function uploadVideo(string memory _videoHash, string memory _title) public {
-    // Make sure the video hash exists
-
-    // Make sure video title exists
-
-    // Make sure uploader address exists
-
-
-    // Increment video id
-
-    // Add video to the contract
-
-    // Trigger an event
-
-  }
+      // Add video to the contract
+      videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
+      emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
+      // Trigger an event
+   }
 }

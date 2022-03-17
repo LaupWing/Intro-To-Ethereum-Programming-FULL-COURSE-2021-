a78df18acecc9f2ce049fa66058cd48db99d7a3c
LaupWing <loc_strict@hotmail.com>
2022-03-17 19:15:52 +0100

    update

--- dvideo/src/components/Main.js
+++ dvideo/src/components/Main.js
@@ -47,17 +47,26 @@ class Main extends Component {
                      <button type='submit' className='btn btn-danger btn-block btn-sm'>Upload!</button>
                      &nbsp;
                   </form>
-                  {/* Map Video...*/}
-                  {/* Return Video...*/}
-                  <div style={{ width: '175px' }}>
-                     <div className="card-title bg-dark">
-                        <small className="text-white"><b>{/*Video title*/}</b></small>
+                  {this.props.videos.map((video, key)=>(
+                     <div 
+                        className='card mb-4 text-center bg-secondary mx-auto'
+                        style={{
+                           width: '175px'
+                        }}
+                     >
+                        <div className="card-title bg-dark">
+                           <small className="text-white"><b>{video.title}</b></small>
+                        </div>
+                        <div>
+                           <p onClick={()=> this.props.changeVideo(video.hash, video.title)}>
+                              <video 
+                                 src={`https://ipfs.infura.io/ipfs/${video.hash}`}
+                                 style={{width: '150px'}}
+                              ></video>
+                           </p>
+                        </div>
                      </div>
-                     <div>
-                        {/* Change Video...*/}
-                        {/* Return Side Videos...*/}
-                     </div>
-                  </div>
+                  ))}
                </div>
             </div>
          </div>
